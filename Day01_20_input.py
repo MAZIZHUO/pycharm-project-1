3b8e740d8d2593b28d9dfff4fcac3fb0b1c74cd7
MAZIZHUO <mazizhuo123@gmail.com>
2025-07-18 17:40:57 +0800

    新增input

--- Day01_20_input.py
+++ Day01_20_input.py
@@ -10,7 +10,7 @@ bmi = weight / (height / 100) ** 2
 print(bmi)
 print(f"bmi = {bmi}")
 print(f"bmi完整 = {bmi}")  # 这是普通的 f-string，字符串里写了字面内容 "bmi = "，然后用 {bmi} 插入变量值。
-print(f"{bmi = }")  # 这是 Python 3.8+ 的调试用格式，自动帮你输出变量名和变量值
+print(f"{bmi = }")        # 这是 Python 3.8+ 的调试用格式，自动帮你输出变量名和变量值
 print(f"{bmi = :.2f}")
 print(round(bmi, 2))
 if 18.5 <= bmi < 24:
