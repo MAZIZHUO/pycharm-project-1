97a38d8c2aabbba3ab544f59484570949e4f5c96
MAZIZHUO <mazizhuo123@gmail.com>
2025-08-12 10:55:19 +0800

    新增

--- Python 100 days/Day01_20_05分支结构.py
+++ Python 100 days/Day01_20_05分支结构.py
@@ -1,3 +1,4 @@
+#以下为if结构
 """
 BMI计算器
 
@@ -22,4 +23,43 @@ print(f'A = {a}') # 这是普通的 f-string
 print(f'A = {a :.2f}')  #:是格式说明符（format specifier） 的分隔符，{变量:格式} → 用冒号 : 后面的格式规则来控制输出方式
 print(f'A = {a = }')  # 这是 Python 3.8+ 的调试用格式，自动帮你输出变量名和变量值
 print(f'A = {a = :.2f}')
+
+#以下为 if-else 结构
+h = 175
+w = 70
+b = w / (h / 100) ** 2
+if 18.5 <= b < 24:
+    print(f'你的BMI是{b :.2f},你的身材很棒!')
+else:
+    print('你的身材不够标准哟！')
+
+#以下为if-elif-else结构
+status_code = int(input('响应状态码: '))
+if status_code == 400:
+    description = 'Bad Request'
+elif status_code == 401:
+    description = 'Unauthorized'
+elif status_code == 403:
+    description = 'Forbidden'
+elif status_code == 404:
+    description = 'Not Found'
+elif status_code == 405:
+    description = 'Method Not Allowed'
+elif status_code == 418:
+    description = 'I am a teapot'
+elif status_code == 429:
+    description = 'Too many requests'
+else:
+    description = 'Unknown status Code'
+print('状态码描述:', description)
+
+#if-else案例
+x = float(input('x = '))
+if x < 0:
+    y = x + 1
+elif x == 0:
+    y = x
+else:
+    y = x / (x + 1)
+print(f'y = {y}')
 
